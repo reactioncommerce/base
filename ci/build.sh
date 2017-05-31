200faf90cec716e239a63685294f24a7073f71c0
Jeremy Shimko <jeremy.shimko@gmail.com>
2017-05-31 13:53:24 -0400

    fix CI tagging

--- ci/build.sh
+++ ci/build.sh
@@ -9,17 +9,5 @@ docker build -t $IMAGE_NAME:base .
 docker build -f dev.dockerfile -t $IMAGE_NAME:devbuild .
 docker build -f prod.dockerfile -t $IMAGE_NAME:latest .
 
-# create a versioned tag for the base image
-docker tag $IMAGE_NAME:base $IMAGE_NAME:base-$CIRCLE_TAG
-
-# point the other two Dockerfiles at the versioned base image
-sed -i.bak "s/:base/:base-$CIRCLE_TAG/" dev.dockerfile
-sed -i.bak "s/:base/:base-$CIRCLE_TAG/" prod.dockerfile
-
-# create the versioned builds
-docker build -f dev.dockerfile -t $IMAGE_NAME:devbuild-$CIRCLE_TAG .
-docker build -f prod.dockerfile -t $IMAGE_NAME:$CIRCLE_TAG .
-
 # create the lean build
 docker build -f lean.dockerfile -t $IMAGE_NAME:lean .
-docker tag $IMAGE_NAME:lean $IMAGE_NAME:$CIRCLE_TAG-lean
--- ci/deploy.sh
+++ ci/deploy.sh
@@ -4,6 +4,18 @@ set -e
 
 IMAGE_NAME=${DOCKER_IMAGE_NAME:-"reactioncommerce/base"}
 
+# create a versioned tag for the base image
+docker tag $IMAGE_NAME:base $IMAGE_NAME:base-$CIRCLE_TAG
+
+# point the other two Dockerfiles at the versioned base image
+sed -i.bak "s/:base/:base-$CIRCLE_TAG/" dev.dockerfile
+sed -i.bak "s/:base/:base-$CIRCLE_TAG/" prod.dockerfile
+
+# create the versioned builds
+docker build -f dev.dockerfile -t $IMAGE_NAME:devbuild-$CIRCLE_TAG .
+docker build -f prod.dockerfile -t $IMAGE_NAME:$CIRCLE_TAG .
+docker tag $IMAGE_NAME:lean $IMAGE_NAME:$CIRCLE_TAG-lean
+
 # login to Docker Hub
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 
