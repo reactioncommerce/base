135d89c7ea4df2947301fa494594bd1e9351a8cf
Jeremy Shimko <jeremy.shimko@gmail.com>
2017-05-31 12:21:24 -0400

    unzip Meteor with bsdtar (fix k8s build issue)

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -7,6 +7,7 @@ printf "\n[-] Installing base OS dependencies...\n\n"
 apt-get update -y
 apt-get install -y --no-install-recommends \
   build-essential \
+  bsdtar \
   bzip2 \
   ca-certificates \
   curl \
