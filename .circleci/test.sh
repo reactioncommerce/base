adfe16537cf269e5ddebd43cc203d7efba2f04ca
Seun Martins <seundotme@gmail.com>
2018-02-28 17:18:59 +0100

    Use test confirming meteor installed

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -5,23 +5,12 @@ set -e
 # load cache from build job
 docker load < ~/docker-cache/image.tar
 
-# download Reaction to test the build
-git clone https://github.com/reactioncommerce/reaction.git /tmp/reaction
+METEOR_VERSION=$(exec docker run --rm reactioncommerce/base:latest meteor --version)
 
-# make sure we have a compatible Dockerfile in the Reaction dir
-cp .circleci/test.Dockerfile /tmp/reaction/Dockerfile
+if [[ $METEOR_VERSION =~ ^Meteor ]]; then
+  echo "Meteor install confirmed"
+  exit 0;
+fi
 
-cd /tmp/reaction
-
-# build new image
-docker build \
-  --build-arg TOOL_NODE_FLAGS="--max-old-space-size=4096" \
-  -t reaction-test .
-
-# run the container and wait for it to boot
-docker-compose -f .circleci/docker-compose.yml up -d
-sleep 30
-
-# use curl to ensure the app returns 200's
-docker exec reaction bash -c "apt-get update && apt-get install -y curl && \
-  curl --retry 10 --retry-delay 10 -v http://localhost:3000"
+echo "Meteor install not found"
+exit 1;
